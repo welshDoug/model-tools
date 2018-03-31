a571b8f04306a3b4aef595078f701830b92b621c
Doug <Cowie>
2018-03-31 16:08:02 +0100

    initial first dates

---
+++ dist/main.js
@@ -0,0 +1,58 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const csvtojson_1 = require("csvtojson");
+const fs = require("fs");
+const json2csv_1 = require("json2csv");
+// create a new converter object
+const converter = new csvtojson_1.Converter({});
+const json2csv = new json2csv_1.Parser();
+// Load Data
+converter.fromFile("./data/dates.csv", function (err, result) {
+    // if an error has occured then handle it
+    if (err) {
+        console.log("An Error Has Occured");
+        console.log(err);
+    }
+    let dateMatrix = calculateFirstNeolithic(result);
+    let values = convertHashtoJSON(dateMatrix);
+    const csv = json2csv.parse(values);
+    fs.writeFile('./data/leads.csv', csv, { encoding: 'utf8' }, (err) => {
+        if (err)
+            throw err;
+        console.log("file written");
+    });
+});
+function extractDate(date) {
+    return date.C14Age;
+}
+function extractRectID(date) {
+    return date.RectID;
+}
+function convertHashtoJSON(map) {
+    let array = new Array();
+    for (let [k, v] of map) {
+        let obj = new Object();
+        obj['RectID'] = k;
+        obj['Date'] = v;
+        array.push(obj);
+    }
+    return array;
+}
+function calculateFirstNeolithic(dates) {
+    return dates.reduce((acc, date, i) => {
+        const rectID = extractRectID(date);
+        const value = extractDate(date);
+        //TODO: Check the date is "Neolithic"
+        const currentVal = acc.get(rectID);
+        if (currentVal == undefined) {
+            acc.set(rectID, value);
+        }
+        else {
+            if (value > currentVal) {
+                //If the new value is earlier replace it
+                acc.set(rectID, value);
+            }
+        }
+        return acc;
+    }, new Map());
+}
---
+++ src/main.ts
@@ -0,0 +1,63 @@
+import {Converter} from 'csvtojson'; 
+import * as fs from 'fs';
+import {Parser} from 'json2csv';
+// create a new converter object
+const converter = new Converter({});
+const json2csv = new Parser();
+
+
+// Load Data
+converter.fromFile("./data/dates.csv",function(err,result){
+    // if an error has occured then handle it
+    if(err){
+        console.log("An Error Has Occured");
+        console.log(err);  
+    } 
+    let dateMatrix = calculateFirstNeolithic(result);
+    let values = convertHashtoJSON(dateMatrix);
+    const csv = json2csv.parse(values);
+    fs.writeFile('./data/firstDates.csv', csv, { encoding: 'utf8' }, (err) => {
+        if (err) throw err;
+        console.log("file written");
+    });
+});
+
+function extractDate(date) {
+    return date.C14Age;
+}
+
+function extractRectID(date) {
+    return date.RectID;
+}
+
+function convertHashtoJSON(map) {
+    let array = new Array();
+    for (let [k,v] of map) {
+        let obj = new Object();
+        obj['RectID'] = k;
+        obj['Date'] = v;
+        array.push(obj);
+    }
+    
+    return array;
+}
+
+function calculateFirstNeolithic(dates) {
+    return dates.reduce((acc, date, i) => {
+        const rectID = extractRectID(date);
+        const value = extractDate(date);
+        
+        //TODO: Check the date is "Neolithic"
+        const currentVal = acc.get(rectID);
+        if (currentVal == undefined) {
+            acc.set(rectID, value);
+        }
+        else {            
+            if (value > currentVal) {
+                //If the new value is earlier replace it
+                acc.set(rectID, value);
+            }
+        }
+        return acc;
+    }, new Map());
+}
